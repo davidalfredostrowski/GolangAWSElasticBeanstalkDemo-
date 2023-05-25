a78de57fd6ba99c310f08c89a88b0fb92f45aa13
davidalfredostrowski <davidaostrowski@gmail.com>
2023-05-24 21:45:42 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+
+func main(){ 
+	http.HandleFunc("/", index)
+	fmt.Println("Now serving localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request){ 	
+	fmt.Fprintf(w, "Hello, this is a web page! %s", time.Now())
+}
+
